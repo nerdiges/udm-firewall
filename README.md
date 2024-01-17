5b4b587f80db1c43be20351d18e9babc18c4550b
Stephan Klein <stephan@nerdig.es>
2024-01-17 21:57:14 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
-# udm-cronjobs
-Benutzerdefinierte Cronjobs beim Systemsart der UDM Pro anlegen
+# udm-firewall
+Regelwerk zur Trennung der Corporate LANS und Guest-Netzwerke, sowie Deaktivierung von NAT bei der UDM Pro.
+
+Werden auf der UDM/UDM Pro mit unifiOS Version 3.x unterschiedliche VLANs verwendet, so kann innerhalb der Sicherheitszonen LAN und Guest zwischen den jeweiligen VLANs ungefiltert kommuniziert werden. Das Firewallregelwerk im Default keine strikte Trennung umsetzt (siehe auch https://nerdig.es/udm-pro-netzwerktrennung-1/). Damit zwischen den VLANS der Datenverkehr eingeschränkt wird, müssen die Filterregeln entsprechend manuell konfiguriert werden. Dazu können Filterregeln in der GUI genutzt werden (siehe z.B. https://ubiquiti-networks-forum.de/wiki/entry/99-firewall-regeln-2-0-by-defcon/). Während das für IPv4 über die RFC1918 Netzwerke über eine Regel für alle Interface realisiert werden kann, ist das bei IPv6 so einfach nicht möglich. Hier müsste für jede Interface kombinsation eine separate Regel angelegt werden. Zusätzlich muss das IPv6-Regelwerk angepasst werden, wenn z.B. ein neues NEtzwerk angelegt wird. Außerdem gibt es in der GUI noch immer keine Option zur Deaktivierung des vorkonfigurierten NAT für IPv4.
+
+Mit diesem Script wird das Standard-Regelwerk der UDM-Pro entsprechend automatisch angepasst.
+
+## Voraussetzungen
+Unifi Dream Machine Pro mit UnifiOS Version 3.x. Erfolgreich getestet mit UnifiOS 3.2.7.
+
+## Funktionsweise
+Das Script `udm-wireguard.sh` wird bei jedem Systemstart und anschließend alle 90 Sekunden per systemd ausgeführt. Da die von Script erzeugten Firewall-Regeln bei Änderungen an der Netzwerkkonfiguration über die GUI wieder gelöscht werden, wird regelmäßig überprüft, ob die Firewall-Regeln noch passen. Neben dem systemd-Service wird daher auch ein systemd-Timer eingerichtet der das Script alle 90 Sekunden neu startet und die Regeln bei Bedarf wiederherstellt.
+
+## Features
+- Generiert Regeln zur Trennung auf der unterschiedlichen LAN- und Guest-VLANs (IPv4 und IPv6)
+- Deaktivierung der vordefinierten NAT-Regeln
+- Einfügen von Related/Established-Regeln um das Firewall-MAnagement zu vereinfachen
+- Ausführen von weiteren Scripten vor und/oder nachdem das Firewall-Regelwerk angepasst wurde 
+
+
+## Installation
+Nachdem eine Verbindung per SSH zur UDM/UDM Pro hergestellt wurde wird udm-wireguard folgendermaßen installiert:
+
+**1. Download der Dateien**
+```
+mkdir -p /data/custom
+dpkg -l git || apt install git
+git clone https://github.com/nerdiges/udm-firewall.git /data/custom/firewall
+chmod +x /data/custom/wireguard/udm-firewall.sh
+```
+
+**2. Parameter im Script anpassen (optional)**
+
+Siehe Absatz **Konfiguration**.
+
+**3. Einrichten der systemd-Services**
+```
+# Install udm-firewall.service und timer definition file in /etc/systemd/system via:
+ln -s /data/custom/firewall/udm-firewall.service /etc/systemd/system/udm-firewall.service
+ln -s /data/custom/firewall/udm-firewall.timer /etc/systemd/system/udm-firewall.timer
+
+# Reload systemd, enable and start the service and timer:
+systemctl daemon-reload
+systemctl enable udm-firewall.service
+systemctl start udm-firewall.service
+systemctl enable udm-firewall.timer
+systemctl start udm-firewall.timer
+
+# check status of service and timer
+systemctl status udm-firewall.timer udm-firewall.service
+```
+
+## Konfiguration
+**Default-Einstellungen:** 
+
+Wurden in der *Unifi Network* Oberfläche zwei Corporate-Network VLANs mit den VLAN-IDs 20 und 21 konfiguriert, so werden in UnifiOS die Interfaces *br20* und *br21* angelegt. Der Traffic *br20* -> *br21* wird dabei grundsätzlich zugelassen (siehe `$exclude`). 
+
+Es werden außerdem Firewall-Regeln erstellt, die das Connection-Tracking aktiviert und Pakete mit dem Status `established`und `related` zulässt (siehe  `$allow_related_lan`und `$allow_related_guest`).
+
+Ist das Script [udm-wireguard](https://github.com/nerdiges/udm-wireguard) installiert, so wird das Script vor der ANpassung der Firewall-REgeln ausgeführt, damit auch die Wireguard-Interfaces entsprechend geschützt werden (siehe `$commands_before`). Nach der Anpassung
+
+
+Die Konfiguration kann im Script über folgende Variablen angepasst werden:
+```
+######################################################################################
+#
+# Configuration
+#
+
+# interfaces listed in exclude will not be separted and can still access
+# the other VLANs. Multiple interfaces are to be separated by spaces.
+exclude="br20"
+
+# Add rule to allow established and related network traffic coming in to LAN interface
+allow_related_lan=true
+
+# Add rule to allow established and related network traffic coming in to guest interface
+allow_related_guest=true
+
+# Remove predefined NAT rules 
+disable_nat=true
+
+
+# List of commands that should be executed before firewall rules are adopted (e.g. setup 
+# wireguard interfaces, before adopting ruleset to ensure wireguard interfaces are 
+# considerd when  separating VLANs).
+# It is recommended to use absolute paths for the commands.
+commands_before=(
+    "[ -x /data/custom/wireguard/udm-wireguard.sh ] && /data/custom/wireguard/udm-wireguard.sh"
+    ""
+)
+
+
+# List of commands that should be executed after firewall rules are adopted.
+# It is recommended to use absolute paths for the commands.
+commands_after=(
+    ""
+)
+
+#
+######################################################################################
+```
+
+
+
+Siehe auch: https://nerdig.es/udm-pro-netzwerktrennung-2/ und https://nerdig.es/udm-pro-3-upgrade/ 
+
---
+++ udm-firewall.sh
@@ -0,0 +1,246 @@
+#!/bin/bash
+
+######################################################################################
+#
+# Description:
+# ------------
+#	If different corporate and guest networks are defined Unifi Network, these 
+#   networks ar not separated in default configuaration, as a default allow
+#   principle is implemented on UDM Pro. Thus, coporate LAN to corporate LAN 
+#	and guest LAN to guest LAN traffic is allowed per default. In addition it is 
+#	not possible to restrict inter corporate and guest IPv6 traffic via GUI. 
+#   This script adds some rules to sepearte all defined LAN and Guest networks 
+#	(see https://nerdig.es/udm-pro-netzwerktrennung-1/). 
+#	As firewall rules may be reseted whenever ruleset is changed in GUI, this
+#   file should be executed regularly (e.g. via systemd timer or cron) to ensure 
+#	that firewall is permanently activated.
+#
+######################################################################################
+
+######################################################################################
+#
+# Configuration
+#
+
+# interfaces listed in exclude will not be separted and can still access
+# the other VLANs. Multiple interfaces are to be separated by spaces.
+exclude="br20"
+
+# Add rule to allow established and related network traffic coming in to LAN interface
+allow_related_lan=true
+
+# Add rule to allow established and related network traffic coming in to guest interface
+allow_related_guest=true
+
+# Remove predefined NAT rules 
+disable_nat=true
+
+
+# List of commands that should be executed before firewall rules are adopted (e.g. setup 
+# wireguard interfaces, before adopting ruleset to ensure wireguard interfaces are 
+# considerd when  separating VLANs).
+# It is recommended to use absolute paths for the commands.
+commands_before=(
+    "[ -x /data/custom/wireguard/udm-wireguard.sh ] && /data/custom/wireguard/udm-wireguard.sh"
+    ""
+)
+
+
+# List of commands that should be executed after firewall rules are adopted.
+# It is recommended to use absolute paths for the commands.
+commands_after=(
+    ""
+)
+
+#
+######################################################################################
+
+
+######################################################################################
+#
+# No further changes should be necessary beyond this line.
+#
+
+# set scriptname
+me=$(basename $0)
+
+# Exectue Scripts defined in $commands_before
+for cmd in "${commands_before[@]}"; do
+    eval "$cmd"
+done
+
+
+# Buffer IPv4 ruleset
+ipv4rules=$(/usr/sbin/iptables --list-rules)
+function in_ip4rules () { [[ $ipv4rules =~ "$1" ]] || return 1; }
+
+# Buffer IPv6 ruleset
+ipv6rules=$(/usr/sbin/ip6tables --list-rules)
+function in_ip6rules () { [[ $ipv6rules =~ "$1" ]] || return 1; }
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# LAN separation
+#
+
+# Get list of relevant LAN interfaces and total number of interfaces
+lan_if=$(echo -e "$ipv4rules" | /usr/bin/awk '/^-A UBIOS_FORWARD_IN_USER.*-j UBIOS_LAN_IN_USER/ { print $4 }')
+lan_if_count=$(echo $lan_if | /usr/bin/wc -w)
+
+# add allow related/established to UBIOS_LAN_IN_USER if requested
+if [ $allow_related_lan == "true" ]; then
+    rule="-A UBIOS_LAN_IN_USER -m conntrack --ctstate RELATED,ESTABLISHED.*-j RETURN"
+    in_ip4rules "$rule" || /usr/sbin/iptables -I UBIOS_LAN_IN_USER 1 -m conntrack --ctstate RELATED,ESTABLISHED -j RETURN
+    in_ip6rules "$rule" || /usr/sbin/ip6tables -I UBIOS_LAN_IN_USER 1 -m conntrack --ctstate RELATED,ESTABLISHED -j RETURN
+fi
+
+# LAN separation only necessary if at least 2 LANs are configured
+if [ $lan_if_count -gt 1 ]; then
+
+    # prepare ip(6)tables chains lan_separation
+    in_ip4rules "-N lan_separation" || (/usr/sbin/iptables -N lan_separation &> /dev/null  && /usr/bin/logger "$me: IPv4 chain created (lan_separation)")
+    in_ip6rules "-N lan_separation" || (/usr/sbin/ip6tables -N lan_separation &> /dev/null && /usr/bin/logger "$me: IPv6 chain created (lan_separation)")
+
+    # Add rules to separate LAN-VLANs to chain lan_separation
+    for i in $lan_if; do
+        case "$exclude " in 
+            *"$i "*)
+                /usr/bin/logger "$me: Excluding $i from LAN separation as requested in config."
+            ;;
+
+            *)
+            for o in $lan_if; do
+                if ! [ "$i" == "$o" ]; then
+                    rule="-A lan_separation -i $i -o $o -j REJECT"
+                    in_ip4rules "$rule" || /usr/sbin/iptables $rule
+                    in_ip6rules "$rule" || /usr/sbin/ip6tables $rule
+                fi
+            done
+            ;;
+        esac
+    done 
+
+    # add IPv4 rule to include rules in chain lan_separation
+    if ! in_ip4rules "-A UBIOS_LAN_IN_USER -j lan_separation" ; then
+        rules=$(/usr/sbin/iptables -L UBIOS_LAN_IN_USER --line-numbers | /usr/bin/awk 'END { print $1 }')
+        v4_idx=$(/usr/bin/expr $rules - $lan_if_count)
+        /usr/sbin/iptables -I UBIOS_LAN_IN_USER $v4_idx -j lan_separation
+    fi 
+
+    # add IPv6 rule to include rules in chain lan_separation
+    if ! in_ip6rules "-A UBIOS_LAN_IN_USER -j lan_separation" ; then
+        v6_idx=$(/usr/sbin/ip6tables -L UBIOS_LAN_IN_USER --line-numbers | /usr/bin/awk '/match-set UBIOS_ALL_NETv6_br[0-9]+ src \/\*/{ print $1; exit}')
+        /usr/sbin/ip6tables -I UBIOS_LAN_IN_USER $v6_idx -j lan_separation
+    fi
+else
+    /usr/bin/logger "$me: Just one LAN interface detected. No REJECT-Rules to separate LANs implemented."
+        
+    if in_ip4rules "-N lan_separation"; then
+        /usr/sbin/iptables -F lan_separation && /usr/bin/logger "$me: Existing IPv4 chain lan_separation flushed."
+        /usr/sbin/iptables -X lan_separation && /usr/bin/logger "$me: Existing IPv4 chain lan_separation deleted."
+    fi
+
+    if in_ip6rules "-N lan_separation"; then
+        /usr/sbin/ip6tables -F lan_separation && /usr/bin/logger "$me: Existing IPv6 chain lan_separation flushed."
+        /usr/sbin/ip6tables -X lan_separation && /usr/bin/logger "$me: Existing IPv6 chain lan_separation deleted."
+    fi
+fi
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# guest separation
+#
+
+# Get list of relevant guest interfaces and total number of interfaces
+guest_if=$(echo -e "$ipv4rules" | /usr/bin/awk '/^-A UBIOS_FORWARD_IN_USER.*-j UBIOS_GUEST_IN_USER/ { print $4 }')
+guest_if_count=$(echo $guest_if | /usr/bin/wc -w)
+
+# add allow related/established to UBIOS_LAN_IN_USER if requested
+if [ $allow_related_guest == "true" ]; then
+    rule="-A UBIOS_GUEST_IN_USER -m conntrack --ctstate RELATED,ESTABLISHED.*-j RETURN"
+    in_ip4rules "$rule" || /usr/sbin/iptables -I UBIOS_GUEST_IN_USER 1 -m conntrack --ctstate RELATED,ESTABLISHED -j RETURN
+    in_ip6rules "$rule" || /usr/sbin/ip6tables -I UBIOS_GUEST_IN_USER 1 -m conntrack --ctstate RELATED,ESTABLISHED -j RETURN
+fi
+
+# Guest separation only necessary if at least 2 Guest networks are configured
+if [ $guest_if_count -gt 1 ]; then
+
+    # prepare ip(6)tables chains guest_separation
+    in_ip4rules "-N guest_separation" || (/usr/sbin/iptables -N guest_separation &> /dev/null && /usr/bin/logger "$me: IPv4 chain created (guest_separation)")
+    in_ip6rules "-N guest_separation" || (/usr/sbin/ip6tables -N guest_separation &> /dev/null && /usr/bin/logger "$me: IPv6 chain created (guest_separation)")
+
+    # Add rules to chain guest_separation
+    for i in $guest_if; do
+        case "$exclude " in
+            *"$i "*)
+                /usr/bin/logger "$me: Excluding $i from guest VLAN separation as requested in config."
+            ;;
+
+            *)
+                for o in $guest_if; do
+                    if ! [ "$i" == "$o" ]; then
+                        rule="-A guest_separation -i $i -o $o -j REJECT"
+                        in_ip4rules "$rule" || /usr/sbin/iptables $rule
+                        in_ip6rules "$rule" || /usr/sbin/ip6tables $rule
+                    fi
+                done
+            ;;
+        esac
+    done
+
+    if ! in_ip6rules "-A UBIOS_GUEST_IN_USER -j guest_separation" ; then
+        # add IPv4 rule to include rules in chain guest_separation
+        rules=$(/usr/sbin/iptables -L UBIOS_GUEST_IN_USER --line-numbers | /usr/bin/awk 'END { print $1 }')
+        v4_idx=$(expr $rules - $guest_if_count)
+        /usr/sbin/iptables -I UBIOS_GUEST_IN_USER $v4_idx -j guest_separation
+    fi
+
+    # add IPv6 rule to include rules in chain guest_separation
+    if ! in_ip6rules "-A UBIOS_GUEST_IN_USER -j guest_separation" ; then
+        v6_idx=$(/usr/sbin/ip6tables -L UBIOS_GUEST_IN_USER --line-numbers | /usr/bin/awk '/RETURN.*match-set UBIOS_ALL_NETv6_br[0-9]+ src \/\*/ { print $1; exit }')
+        /usr/sbin/ip6tables -I UBIOS_GUEST_IN_USER $v6_idx -j guest_separation
+    fi
+else
+    /usr/bin/logger "$me: Just one guest interface detected. No filters implemented. Starting clean up..."
+        
+    if in_ip4rules "-N guest_separation"; then
+        /usr/sbin/iptables -F guest_separation && /usr/bin/logger "$me: Existing IPv4 chain guest_separation flushed."
+        /usr/sbin/iptables -X guest_separation && /usr/bin/logger "$me: Existing IPv4 chain guest_separation deleted."
+    fi
+
+    if in_ip6rules "-N guest_separation"; then
+        /usr/sbin/ip6tables -F guest_separation && /usr/bin/logger "$me: Existing IPv6 chain guest_separation flushed."
+        /usr/sbin/ip6tables -X guest_separation && /usr/bin/logger "$me: Existing IPv6 chain guest_separation deleted."
+    fi
+fi
+
+
+# add rules to fix packet leakage from LAN > guest
+for i in $lan_if; do
+    for o in $guest_if; do
+        rule="-A lan_separation -i $i -o $o -j REJECT"
+        in_ip4rules "$rule" || /usr/sbin/iptables $rule
+        in_ip6rules "$rule" || /usr/sbin/ip6tables $rule
+    done
+done
+
+# disable NAT if requested
+if [ $disable_nat == "true" ]; then
+    # identify MASQUERADE jump target in UBIOS_POSTROUTING_USER_HOOK chain
+    # which will be added per default for UBIOS_ADDRv4_ethX (eth8/eth9) to
+    # manage NAT throught WAN
+    rules=$(/usr/sbin/iptables -t nat -L UBIOS_POSTROUTING_USER_HOOK --line-numbers | \
+            /usr/bin/awk '/MASQUERADE .* UBIOS_.*ADDRv4_eth. src/ { print $1 }')
+
+    # for each rule identified we issue a delete operation in reverse
+    # order so that UBIOS_POSTROUTINE_USER_HOOK will really only contain
+    # NAT rules a user manually defined in the Network UI.
+    for rulenum in $(echo -e "${rules}" | /usr/bin/sort -r); do
+        /usr/sbin/iptables -t nat -D UBIOS_POSTROUTING_USER_HOOK ${rulenum}
+    done
+fi
+
+# Exectue Scripts defined in $commands_after
+for cmd in "${commands_after[@]}"; do
+    eval "$cmd"
+done
